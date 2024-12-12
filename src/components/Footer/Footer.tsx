7c77b7a1065ed8449367cdce65740f987af19c3a
nandita-saha <sahan4648@gmail.com>
2024-12-12 18:21:00 +0530

    updated header and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,78 @@
+import React from "react"
+import Link from "next/link"
+import Image from "next/image"
+import styles from './footer.module.css'
+
+
+// import styles from '@/components/Footer/footer.module.css'
+
 export default function Footer() {
-    return <div>Footer</div>
-  }
+  return (
+    <div className="footer flex items-center justify-between">
+      <div>
+        <h5 className={`${styles.foooterHeading}`}>Get in touch</h5>
+        <ul className={`${styles.addressList}`}>
+          <li>
+            <span className={`${styles.footerIcon}`}>
+              <Image className="w-6 h-6" width="100" height="100" src="/assets/images/home/foooter-map-icon.svg" alt="" />
+            </span>
+            <p className="address">
+              141/A, A.C.Road, Khagra, Berhampore, Murshidabad, West Bengal - 742103
+            </p>
+          </li>
+          <li>
+            <span className={`${styles.footerIcon}`}>
+              <Image className="w-6 h-6" width="100" height="100" src="/assets/images/home/foooter-phone-icon.svg" alt="" />
+            </span>
+            <Link href="tel:9775600370">+91 9775600370</Link>
+          </li>
+          <li>
+            <span className={`${styles.footerIcon}`}>
+              <Image className="w-6 h-6" width="100" height="100" src="/assets/images/home/foooter-email-icon.svg" alt="" />
+            </span>
+            <Link href="mailto:info@sholapith.com">info@sholapith.com</Link>
+          </li>
+
+        </ul>
+        <Image className={`${styles.biswaBanglaLogo}`} height="300" width="200" src="/assets/images/webp/biswa-bangla-logo.webp" alt="" />
+      </div>
+
+      <div className="">
+        <div className={`${styles.footerLogo}`}>
+          <Image className="" width="200" height="200" src="/assets/images/home/logo-img.webp" alt="" />
+        </div>
+        <div className={`${styles.footerLogo}`}>
+          <Image className="" width="200" height="200" src="/assets/images/home/footer-text-logo.svg" alt="" />
+        </div>
+      </div>
+
+      <div>
+        <h5 className={`${styles.foooterHeading}`}>Quick Link</h5>
+        <ul className={`${styles.footerQuickLink}`}>
+          <li><Link href="">About Us</Link></li>
+          <li><Link href="">How we work</Link></li>
+          <li><Link href="">Our Prodcuts</Link></li>
+          <li><Link href="">Contact Us</Link></li>
+        </ul>
+        <ul className={`${styles.socialLinks}`}>
+          <li>
+            <Link href="">
+              <Image className="h-6 w-6" height="100" width="100" src="/assets/images/home/facebook.svg" alt="facebook icon" />
+            </Link>
+          </li>
+          <li>
+            <Link href="">
+              <Image className="h-6 w-6" height="100" width="100" src="/assets/images/home/youtube.svg" alt="youtube icon" />
+            </Link>
+          </li>
+          <li>
+            <Link href="">
+              <Image className="h-6 w-6" height="100" width="100" src="/assets/images/home/linkdin.svg" alt="linkedin icon" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,89 @@
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
+import { useState, useEffect } from "react"
+import { useRouter } from "next/router"
+
 import styles from './header.module.css'
 
-export default function Header (){
-  return(
-    <div className={`  ${styles.header}`}>
-      <div className="px-3 flex items-center justify-between ">
-          <Link className="navbar-brand" href="/">
-            <Image width="200" height="100" className="" src="/assets/images/headers/logo-img.webp" alt="logo" />
-          </Link>
-          <div className="">
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
 
+  const toggleMenu = (route: string) => {
+    if (router.pathname === route) {
+      setMenuOpen(false);
+    } else {
+      setMenuOpen(!menuOpen);
+    }
+  };
+
+  // Effect to handle body overflow when the menu is open
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [menuOpen]);
+
+  return (
+
+    <>
+      <div className={`${styles.header} absolute top-0 z-50 px-4 py-2 flex w-full items-center justify-between`}>
+        <Link href="/">
+          <Image width="200" height="200" className="w-32 md:w-40 lg:w-44" src="/assets/images/headers/logo.svg" alt="logo" />
+        </Link>
+
+        <ul className="hidden lg:flex items-center justify-center">
+          <li className="text-center mx-5">
+            <Link className="text-sm md:text-sm text-white" href="/about">About Us</Link>
+          </li>
+          <li className="text-center mx-5">
+            <Link className="text-sm md:text-sm text-white" href="/how-we-work">How We Work</Link>
+          </li>
+          <li className="text-center mx-5">
+            <Link className="text-sm md:text-sm text-white" href="/products">Our Products</Link>
+          </li>
+          <li className="text-center mx-5">
+            <Link className="text-sm md:text-sm text-white" href="/contact">Contact Us</Link>
+          </li>
+          <li className={`${styles.phoneNumber}`}>
+            <Link href="tel:9775600370" className="text-sm md:text-sm flex items-center">
+              <Image src="/assets/images/headers/phone-icon.svg" width="100" height="100" className="w-6 h-6" alt="" /> +91 9775600370
+            </Link>
+          </li>
+        </ul>
+
+
+        <div className="w-6 h-6 lg:w-10 lg:hidden flex items-center justify-center cursor-pointer" onClick={() => toggleMenu("")}>
+          <div className={`${styles.hamburger} ${menuOpen ? `${styles.cross}` : ''}`}>
+            <span></span>
           </div>
+        </div>
+
       </div>
-    </div>
+      <div
+        className={`overlay-menu z-30 fixed inset-0 flex justify-center bg-[#0631a7] transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'
+          } transition-transform duration-300 ease-in-out `}
+      >
+        <ul className="mt-[100px]">
+          <li className="text-center my-2">
+            <Link className="text-lg lg:text-lg text-white hover:text-[#6cd4da]" href="/about" onClick={() => toggleMenu("/about")}>About Us</Link>
+          </li>
+          <li className="text-center my-2">
+            <Link className="text-lg lg:text-lg text-white hover:text-[#6cd4da]" href="/how-we-work" onClick={() => toggleMenu("/how-we-work")}>How We Work</Link>
+          </li>
+          <li className="text-center my-2">
+            <Link className="text-lg lg:text-lg text-white hover:text-[#6cd4da]" href="/products" onClick={() => toggleMenu("/products")}>Our Products</Link>
+          </li>
+          <li className="text-center my-2">
+            <Link className="text-lg lg:text-lg text-white hover:text-[#6cd4da]" href="/contact" onClick={() => toggleMenu("/contact")}>Contact Us</Link>
+          </li>
+        </ul>
+      </div>
+
+    </>
+
   )
 }
