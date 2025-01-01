85984dc7fb8e7698473cc4649f46c8f3402ce4d9
nandita-saha <sahan4648@gmail.com>
2025-01-01 22:29:17 +0530

    about page content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,65 @@
 // pages/index.tsx
 import React from 'react';
+import Link from 'next/link';
 
 export default function About() {
   return (
-    <div className="baseContainer">
-      <h1>Welcome to Our About page!</h1>
-      <p>Discover amazing content here.</p>
-    </div>
+    <>
+      <section className="abtus-sec1">
+        <div className="baseContainer">
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+            <div className='md:col-span-1'>
+
+            </div>
+            <div className='md:col-span-1'>
+              <h2 className="black-blue-text">Enliven Your Ethnic Roots with <span>Wholesome</span> Craftsmanship</h2>
+              <p>
+                Headed by the impressive experience of Smt. Nupur Saha and her foundation Nupur Silpalay, Sholapith found its online establishment as its official online website. Sholapith defines the profound love and passion of Mrs. Saha for the Shola handicrafts, and describes her 25 years of rich experience aesthetically.
+              </p>
+              <p>
+                The original establishment “Nupur Silpalay” got its physical store and workshop registered under the Government liaison of an SSI Unit (Small Scale Industries Unit) under the business category "Arts & Crafts". It is Mrs. Saha’s immense dedication as a proclaimed shola craft artisan, that the organization has ventured into an online community bringing out the finest wholesale and retail shola handicrafts all over the country.
+              </p>
+
+            </div>
+          </div>
+          <p>
+            Having a humble beginning in 2012 at Berhampore, Murshidabad in West Bengal, this dream venture of Mrs. Saha set its stage into the market with the likes of Kadam, Dolungi, Roses and Chandramallika flowers, garlands, and Bengali wedding accessories such as Topor-Mukut, all created out of shola wood craft.
+          </p>
+          <p>
+            It was later in the year 2013 that the District Industries Centre of West Bengal honoured the organization to be a coveted partner and provider of the shola craft work. This opportunity paved new horizons for the organization and helped it venture into designing and merchandizing new sholacraft models including Paltola Nouka, Mayurpankhi and howdah carriages.
+          </p>
+          <p>
+            Sholapith is now ready to achieve newer verticals of success and revive the lost traditions and culture of West Bengal through its pan India presence online. Feel free to connect with us right away to know more!
+          </p>
+        </div>
+      </section>
+      <section className="abtus-sec2">
+        <div className="baseContainer">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+            <div className='md:col-span-1'>
+
+            </div>
+            <div className='md:col-span-1'>
+
+            </div>
+            <div className='md:col-span-1'>
+
+            </div>
+          </div>
+          <p>
+            Sholapith is proud to have a fantastic presence with its widest range of creatively crafted shola products. We have mastered the design of over 2000 unique shola wood craft products under more than 15 categories and a rich audience base of 300 loyal customers visiting our physical store regularly.
+          </p>
+          <p>
+            With a deep understanding of the rich cultural heritage we hold, we have committed ourselves in providing our customers with exactly what they are looking for. We have always aimed at providing a piece of our heart with every gift item we design.
+          </p>
+          <Link href="" className="">Get in touch</Link>
+        </div>
+      </section>
+
+      <section>
+        
+      </section>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // pages/index.tsx
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Homepage() {
   return (
@@ -17,14 +18,22 @@ export default function Homepage() {
       </section>
       <section className='culture'>
         <div className="baseContainer">
-          <h2 className='black-blue-text'><span>Sholapith</span> – A Home to the Bengal <span>Culture and Tradition</span></h2>
-          <p>
-            Welcome to the illustrious world of Sholapith, where tradition and culture of the shola craft take an all-new dimension under the able guidance of Mrs. Nupur Saha and her visionary project of Nupur Silpalay.
-          </p>
-          <p>
-            Mesmerize your aesthetic senses with a wide variety of world-class shola handicraft and shola wood craft at the most sought-after shola handicraft store online. Discover the best shola craft of West Bengal right here, right away!
-          </p>
-          <Link href="" className='blue-btn'>Contact Us..</Link>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-10 px-2'>
+            <div className='md:col-span-1 relative'>
+              <Image width="1000" height="1000" className="w-2/3 mx-auto" src="/assets/images/webp/woman-img.webp" alt="logo" />
+            </div>
+            <div className='md:col-span-1'>
+              <h2 className='black-blue-text'><span>Sholapith</span> – A Home to the Bengal <span>Culture and Tradition</span></h2>
+              <p className='mb-3'>
+                Welcome to the illustrious world of Sholapith, where tradition and culture of the shola craft take an all-new dimension under the able guidance of Mrs. Nupur Saha and her visionary project of Nupur Silpalay.
+              </p>
+              <p className='mb-4'>
+                Mesmerize your aesthetic senses with a wide variety of world-class shola handicraft and shola wood craft at the most sought-after shola handicraft store online. Discover the best shola craft of West Bengal right here, right away!
+              </p>
+              <Link href="" className='blue-btn'>Contact Us..</Link>
+            </div>
+          </div>
+
         </div>
       </section>
       <section className='top-picks'>
@@ -55,7 +64,7 @@ export default function Homepage() {
       </section>
       <section className='video'>
         <div className="baseContainer text-center">
-          <h2>A sneakpeak to our Works</h2>
+          <h2>A sneakpeak to our works</h2>
           <p className='black-orange-text '>
             Discover the widest selection of some of the <span>finest art and craft</span> of West Bengal only at Sholapith Craft. <br />
             We are a reputed shola craft shop in <span>Berhampore</span> which thrives in preserving the <br />
@@ -72,8 +81,8 @@ export default function Homepage() {
         <div className="baseContainer text-center">
           <h2 className="black-blue-text">Get <span>Beautifully Customized</span> Shola Craft Work <br />
             at Your <span>Doorsteps</span> Today!</h2>
-          <p>At Sholapith, we promise to deliver every piece of craft and décor with equal dedication and sincerity. We are committed to customize your favourite piece of craft order; despite the numbers you order from our handicraft store online. We have tied up with the best courier partners to safely and swiftly deliver your favourite products.</p>
-          <p>
+          <p className='mb-3'>At Sholapith, we promise to deliver every piece of craft and décor with equal dedication and sincerity. We are committed to customize your favourite piece of craft order; despite the numbers you order from our handicraft store online. We have tied up with the best courier partners to safely and swiftly deliver your favourite products.</p>
+          <p className='mb-4'>
             No matter what you choose, or the quantity of the products, a single one or multiple items in one go, we will customize each of your shola handicraft with same zeal and quality at our workshop in Berhampore, West Bengal. With sheer love and passion towards our work, we assure to deliver nothing but the best-in-class shola work right at your doorsteps anywhere in India!
           </p>
           <Link className="blue-btn" href="">Get in touch</Link>
